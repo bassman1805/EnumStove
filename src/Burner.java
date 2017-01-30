18938b37954493e879f3aa59b558c17255fb50db
Nick <bassman1805@gmail.com>
2017-01-30 11:15:56 -0700

    On branch master  Changes to be committed: 	modified:   src/Burner.java 	new file:   src/Setting.java 	modified:   src/Stove.java
    Added plusButton and minusButton to burner

--- src/Burner.java
+++ src/Burner.java
@@ -1,4 +1,50 @@
-
 public class Burner {
+	public enum Temperature{BLAZING, HOT, WARM, COLD};
+	
+	public static final int TIME_DURATION = 2;
+	
+	private Temperature myTemperature;
+	private Setting mySetting;
+	private int timer;
+	
+	public Burner(){
+		myTemperature = Temperature.COLD;
+		mySetting = Setting.OFF;
+	}
+	
+	public void plusButton(){
+		switch(mySetting){
+			case OFF:
+				mySetting = Setting.LOW;
+				break;
+			case LOW:
+				mySetting = Setting.MEDIUM;
+				break;
+			case MEDIUM:
+				mySetting = Setting.HIGH;
+				break;
+			default:
+				//do nothing
+		}
+	}
+	public void minusButton(){
+		switch(mySetting){
+			case HIGH:
+				mySetting = Setting.MEDIUM;
+				break;
+			case MEDIUM:
+				mySetting = Setting.LOW;
+				break;
+			case LOW:
+				mySetting = Setting.OFF;
+				break;
+			default:
+				//do nothing
+		}
+	}
+	
+	public Temperature getTemp(){
+		return myTemperature;
+	}
 
 }
---
+++ src/Setting.java
@@ -0,0 +1,4 @@
+
+public enum Setting {
+	OFF, LOW, MEDIUM, HIGH
+}
